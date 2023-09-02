96f2b4b815bb6f21229b0acee1c179fb152653a9
M A Ali <mjksoft@gmail.com>
2023-09-02 12:14:43 +0300

    Test Github Update

---
+++ EmployeeApp/GitHubs.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+# Steps How to Create Connection to GitHub
+# git config --global user.name "Your Name"
+# git config --global user.email "you@youraddress.com"
+# git config --global push.default matching
+# git config --global alias.co checkout
+# git init
+# git add .
+# git commit -am "initial commit"
+
+# git remote add origin https://github.com/MjkSoft/DjangoAPI.git
+# git branch -M main
+# git push -u origin main
+
+
