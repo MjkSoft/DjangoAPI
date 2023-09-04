51e6b8312e2bd3f8adc9052f7a5f951c79fb8022
M A Ali <mjksoft@gmail.com>
2023-09-04 22:35:21 +0300

    Add Home About

--- EmployeeApp/admin.py
+++ EmployeeApp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+# from EmployeeApp.views import home, about
 
 # Register your models here.
 from .models import Employees, Departments
 
 admin.site.register(Employees)
 admin.site.register(Departments)
+# .site.register(home)
+# admin.site.register(about)
